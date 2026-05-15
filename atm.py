7928c1ecdeada28e8b2faef8d739f9877ce9c139
Charan Aade <280011215+Charan-Code600@users.noreply.github.com>
2026-05-15 19:32:18 +0530

    Refactor ATM input prompts and messages
    Updated prompts for user input and improved print statements for clarity.

--- atm.py
+++ atm.py
@@ -1,30 +1,26 @@
-balance = 10000
-print("""
-====================
-WELL COME TO HDFC 
-    ATM Machine
-====================
-Balance Check   enter---> 1
-Withdraw        enter---> 2
-Deposit         enter---> 3
-Exit            enter---> 4
 
-""")
+balance = 10000
+pin = ""
+
+while pin != "1234":
+    pin = input("enter pin:")
+
+print("Access Granted")
 
 while True:
     option = (input("Choose: "))
     
     if option == "1":
          print(f"💰 Available Balance: ₹{balance}")
-        
+
     elif option == "2":
-        enter =int(input("enter amount:"))
+        enter =int(input("Withdraw amount:"))
         if enter <= balance :
             balance = balance-enter
             print(f"✅ Withdraw ₹{enter} From HDFC Bank ||    💰 Available Balance: ₹{balance}")
             
     elif option == "3":
-        deposit =int(input("enter deposit:"))
+        deposit =int(input("deposit amount:"))
         if deposit > 0:
             balance += deposit
             print(f"✅ Update! ₹{deposit} deposited in HDFC Bank||  💰 New Balance: ₹{balance}")
@@ -32,3 +28,5 @@ while True:
     elif option == "4":
         print("thank you for transaction ")
         break
+
+    
