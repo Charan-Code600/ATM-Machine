d95a90dfd1d5104de2e1f9f2309a5a199f513cdc
Charan Aade <280011215+Charan-Code600@users.noreply.github.com>
2026-05-16 13:25:54 +0530

    Revise ATM menu and transaction logic

--- atm.py
+++ atm.py
@@ -1,17 +1,24 @@
+
+
+
 print("""
 ====================
 WELL COME TO HDFC 
     ATM Machine
 ====================
-Balance Check   enter---> 1
-Withdraw        enter---> 2
-Deposit         enter---> 3
-Exit            enter---> 4
 
+Balance Check      enter---> 0
+Minimum balance    enter---> 1
+Withdraw           enter---> 2
+deposit            enter---> 3
+Transaction        enter---> 4
+history
+Exit               enter---> 5
+
+Password protected....
 """)
-
-
-balance = 10000
+history = []
+balance = 1000
 pin = ""
 
 while pin != "1234":
@@ -22,21 +29,61 @@ print("Access Granted")
 while True:
     option = (input("Choose: "))
     
-    if option == "1":
-         print(f"💰 Available Balance: ₹{balance}")
+    if option == "0":
+        print(f"💰 Available Balance: ₹{balance}")
+
+    elif option =="1":
+        enter = int(input(" Enter number between 1 to 9  :"))
+        if balance - enter < 1000:
+            print("""
+              
+            ⚠ Minimum balance rule:
+    Your account must always maintain ₹1,000.
+Please add more funds or withdraw a smaller amount.
+                  
+            add more balance to press 3
+              
+            """)
 
     elif option == "2":
-        enter =int(input("Withdraw amount:"))
-        if enter <= balance :
-            balance = balance-enter
-            print(f"✅ Withdraw ₹{enter} From HDFC Bank ||    💰 Available Balance: ₹{balance}")
-            
+        enter = int(input("Withdraw amount:"))
+        if enter <= balance - 1000:  
+            balance = balance - enter
+            history.append(f"Withdrawn: ₹{enter}")
+            print(f"✅ Withdraw ₹{enter} From HDFC Bank || 💰 Available Balance: ₹{balance}")
+        else:  # ✅ यह add करो
+            print(f"""
+⚠ Your account balance is ₹{balance}
+Add more balance to press 3
+        """)
+              
     elif option == "3":
         deposit =int(input("deposit amount:"))
         if deposit > 0:
             balance += deposit
+            history.append(f"Deposited: ₹{deposit}")
             print(f"✅ Update! ₹{deposit} deposited in HDFC Bank||  💰 New Balance: ₹{balance}")
-        
+
     elif option == "4":
-        print("thank you for transaction ")
+        print("\n--- Transaction History ---")
+        if len(history) == 0:
+            print("कोई transaction नहीं हुआ!")
+        else:                          
+            for h in history:
+                print(h)
+
+    elif option == "5":
+        print("Thank you for Transaction")  
         break
+
+
+
+
+
+
+
+
+
+
+
+
